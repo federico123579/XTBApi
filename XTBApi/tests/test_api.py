f022e24b9879e242af7a866aed737fa1a52b88ce
Federico Lolli <federico123579@gmail.com>
2019-03-30 00:15:42 +0100

    Added first commands

--- XTBApi/tests/test_api.py
+++ XTBApi/tests/test_api.py
@@ -5,10 +5,70 @@ tests.test_api.py
 test the api client
 """
 
+import pytest
+
 from XTBApi.api import Client
 
 
-def test_login():
-    client = Client()
-    response = client.get_response()
+@pytest.fixture(scope="module")
+def _get_client():
+    return Client()
+
+
+def test_login(_get_client):
+    client = _get_client
+    response = client.login('10649413', '20072001FdRcLlL')
     print(response)
+    print(response)
+    assert response['status'] is True
+    assert client._stream_id is not None
+
+
+def test_get_balance(_get_client):
+    client = _get_client
+    response = client.get_balance()
+    print(response)
+    assert response is not None
+
+
+def test_get_candles(_get_client):
+    client = _get_client
+    response = client.get_candles('EURUSD')
+    print(response)
+    assert response is not None
+
+
+def test_all_symbols(_get_client):
+    client = _get_client
+    response = client.get_all_symbols()
+    print(response)
+    assert response['status'] is True
+
+
+def test_calendar(_get_client):
+    client = _get_client
+    response = client.get_calendar()
+    print(response)
+    assert response['status'] is True
+
+
+def test_get_commission(_get_client):
+    client = _get_client
+    response = client.get_commission('EURUSD', 1.0)
+    print(response)
+    assert response['status'] is True
+
+
+def test_get_user_data(_get_client):
+    client = _get_client
+    response = client.get_user_data()
+    print(response)
+    assert response['status'] is True
+
+
+# at the end of file
+def test_logout(_get_client):
+    client = _get_client
+    response = client.logout()
+    print(response)
+    assert response['status'] is True
