d05c6196540d2062fec9679b5ebeb02885b2914b
Federico Lolli <federico123579@gmail.com>
2019-03-30 00:15:42 +0100

    Added first commands

--- XTBApi/api.py
+++ XTBApi/api.py
@@ -1,24 +1,96 @@
-#!/usr/bin/env python3
 # -*- coding utf-8 -*-
 
-from websocket import create_connection
+"""
+XTBApi.api
+~~~~~~~
+
+Main module
+"""
+
 import json
+
+from websocket import create_connection
+
+
+def _get_data(command, **parameters):
+    data = {
+        "command": command,
+    }
+    if len(parameters) > 0:
+        data['arguments'] = {}
+        for key, value in parameters.items():
+            data['arguments'][key] = value
+    return data
+
+
+def _get_streaming_data(command, **parameters):
+    data = {
+        "command": command,
+    }
+    for key, value in parameters.items():
+        data[key] = value
+    return data
+
 
 class Client(object):
     """main client class"""
 
-    def __int__(self):
-        pass
+    def __init__(self):
+        self.ws = create_connection("wss://ws.xapi.pro/demo")
+        self.stream_ws = create_connection("wss://ws.xapi.pro/demoStream")
+        self._stream_id = None
 
-    def get_response(self):
-        data = {
-            "command": "login",
-            "arguments": {
-                "userId": "10649413",
-                "password": "***REMOVED***"
-            }
-        }
-        ws = create_connection("wss://ws.xapi.pro/demo")
-        ws.send(json.dumps(data))
-        response = ws.recv()
+    def _send_command(self, dict_data):
+        """send command to api"""
+        self.ws.send(json.dumps(dict_data))
+        response = self.ws.recv()
+        return json.loads(response)
+
+    def _send_streaming_command(self, dict_data):
+        """send streaming command to api"""
+        dict_data['streamSessionId'] = self._stream_id
+        self.stream_ws.send(json.dumps(dict_data))
+        response = self.stream_ws.recv()
+        return json.loads(response)
+
+    def login(self, user_id, password):
+        """login command"""
+        data = _get_data("login", userId=user_id, password=password)
+        response = self._send_command(data)
+        self._stream_id = response['streamSessionId']
         return response
+
+    def logout(self):
+        """logout command"""
+        data = _get_data("logout")
+        return self._send_command(data)
+
+    def get_balance(self):
+        """getBalance streaming command"""
+        data = _get_streaming_data("getBalance")
+        return self._send_streaming_command(data)
+
+    def get_candles(self, symbol):
+        """getCandles streaming command"""
+        data = _get_streaming_data("getCandles", symbol=symbol)
+        return self._send_streaming_command(data)
+
+    def get_all_symbols(self):
+        """getAllSymbols command"""
+        data = _get_data("getAllSymbols")
+        return self._send_command(data)
+
+    def get_calendar(self):
+        """getCalendar command"""
+        data = _get_data("getCalendar")
+        return self._send_command(data)
+
+    def get_commission(self, symbol, vol):
+        """getCommissionDef command"""
+        data = _get_data("getCommissionDef", symbol=symbol, volume=vol)
+        return self._send_command(data)
+
+    def get_user_data(self):
+        """getCurrentUserData command"""
+        data = _get_data("getCurrentUserData")
+        return self._send_command(data)
