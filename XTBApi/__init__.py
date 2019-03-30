9cf2d143de941b0c9472da05834cd07d75fd200f
Federico Lolli <federico123579@gmail.com>
2019-03-30 22:25:38 +0100

    Completed api and tests - add logger Only missing input checks

---
+++ XTBApi/__init__.py
@@ -0,0 +1,41 @@
+import logging.config
+import os.path
+
+from XTBApi.__version__ import __version__
+
+logging.config.dictConfig({
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'deafult': {
+            'format':
+                '%(asctime)s - %(levelname)s - %(name)s - %(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'deafult',
+        },
+        'rotating': {
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'formatter': 'deafult',
+            'filename': os.path.join(
+                os.path.dirname(__file__), 'logs/logfile.log'),
+            'when': 'midnight',
+            'backupCount': 3
+        }
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'CRITICAL',
+            'propagate': True
+        },
+        'XTBApi': {
+            'handlers': ['rotating'],
+            'level': 'DEBUG'
+        }
+    }
+})
--- XTBApi/tests/test_api.py
+++ XTBApi/tests/test_api.py
@@ -5,9 +5,18 @@ tests.test_api.py
 test the api client
 """
 
+import logging
+import time
+
 import pytest
 
 from XTBApi.api import Client
+
+LOGGER = logging.getLogger('XTBApi.test_api')
+
+USERID = '10649413'
+PASSWORD = '20072001FdRcLlL'
+DEFAULT_CURRENCY = 'EURUSD'
 
 
 @pytest.fixture(scope="module")
@@ -17,52 +26,162 @@ def _get_client():
 
 def test_login(_get_client):
     client = _get_client
-    response = client.login('10649413', '20072001FdRcLlL')
-    print(response)
-    print(response)
+    response = client.login(USERID, PASSWORD)
+    LOGGER.debug("passed")
     assert response['status'] is True
     assert client._stream_id is not None
-
-
-def test_get_balance(_get_client):
-    client = _get_client
-    response = client.get_balance()
-    print(response)
-    assert response is not None
-
-
-def test_get_candles(_get_client):
-    client = _get_client
-    response = client.get_candles('EURUSD')
-    print(response)
-    assert response is not None
 
 
 def test_all_symbols(_get_client):
     client = _get_client
     response = client.get_all_symbols()
-    print(response)
+    LOGGER.debug("passed")
     assert response['status'] is True
 
 
-def test_calendar(_get_client):
+def test_get_calendar(_get_client):
     client = _get_client
     response = client.get_calendar()
-    print(response)
+    LOGGER.debug("passed")
+    assert response['status'] is True
+
+
+def test_get_chart_last_request(_get_client):
+    client = _get_client
+    start = (time.time() - 3600*24) * 1000
+    args = [DEFAULT_CURRENCY, 1440, start]
+    response = client.get_chart_last_request(*args)
+    LOGGER.debug("passed")
+    assert response['status'] is True
+
+
+def test_get_chart_range_request(_get_client):
+    client = _get_client
+    start = (time.time() - 3600*24*2) * 1000
+    end = (time.time() - 3600*24) * 1000
+    args = [DEFAULT_CURRENCY, 1440, start, end, 0]
+    response = client.get_chart_range_request(*args)
+    LOGGER.debug("passed")
     assert response['status'] is True
 
 
 def test_get_commission(_get_client):
     client = _get_client
-    response = client.get_commission('EURUSD', 1.0)
-    print(response)
+    response = client.get_commission(DEFAULT_CURRENCY, 1.0)
+    LOGGER.debug("passed")
     assert response['status'] is True
 
 
 def test_get_user_data(_get_client):
     client = _get_client
     response = client.get_user_data()
-    print(response)
+    LOGGER.debug("passed")
+    assert response['status'] is True
+
+
+def test_get_margin_level(_get_client):
+    client = _get_client
+    response = client.get_margin_level()
+    LOGGER.debug("passed")
+    assert response['status'] is True
+
+
+def test_get_margin_trade(_get_client):
+    client = _get_client
+    response = client.get_margin_trade(DEFAULT_CURRENCY, 1.0)
+    LOGGER.debug("passed")
+    assert response['status'] is True
+
+
+def test_get_profit_calculation(_get_client):
+    client = _get_client
+    args = [DEFAULT_CURRENCY, 0, 1.0, 1.2233, 1.3000]
+    response = client.get_profit_calculation(*args)
+    LOGGER.debug("passed")
+    assert response['status'] is True
+
+
+def test_get_server_time(_get_client):
+    client = _get_client
+    response = client.get_server_time()
+    LOGGER.debug("passed")
+    assert response['status'] is True
+
+
+def test_symbol(_get_client):
+    client = _get_client
+    response = client.get_symbol(DEFAULT_CURRENCY)
+    LOGGER.debug("passed")
+    assert response['status'] is True
+
+
+def test_get_tick_prices(_get_client):
+    client = _get_client
+    args = [[DEFAULT_CURRENCY], time.time() - 3600*24, 0]
+    response = client.get_tick_prices(*args)
+    LOGGER.debug("passed")
+    assert response['status'] is True
+
+
+def test_get_trade_records(_get_client):
+    client = _get_client
+    response = client.get_trade_records([7489839])
+    LOGGER.debug("passed")
+    assert response['status'] is True
+
+
+def test_get_trades(_get_client):
+    client = _get_client
+    response = client.get_trades(True)
+    LOGGER.debug("passed")
+    assert response['status'] is True
+
+
+def test_get_trades_history(_get_client):
+    client = _get_client
+    args = [time.time() - 3600*24, 0]
+    response = client.get_trades_history(*args)
+    LOGGER.debug("passed")
+    assert response['status'] is True
+
+
+def test_get_trading_hours(_get_client):
+    client = _get_client
+    response = client.get_trading_hours([DEFAULT_CURRENCY])
+    LOGGER.debug("passed")
+    assert response['status'] is True
+
+
+def test_get_version(_get_client):
+    client = _get_client
+    response = client.get_version()
+    LOGGER.debug("passed")
+    assert response['status'] is True
+
+
+def test_ping(_get_client):
+    client = _get_client
+    response = client.ping()
+    LOGGER.debug("passed")
+    assert response['status'] is True
+
+
+def test_trade_transaction(_get_client):
+    client = _get_client
+    price = client.get_symbol(DEFAULT_CURRENCY)['returnData']['ask']
+    args = [DEFAULT_CURRENCY, 0, 0, 5.0, 0, price, 0]
+    response = client.trade_transaction(*args)
+    LOGGER.debug("passed")
+    assert response['status'] is True
+
+
+def test_trade_transaction_status(_get_client):
+    client = _get_client
+    price = client.get_symbol(DEFAULT_CURRENCY)['returnData']['ask']
+    args = [DEFAULT_CURRENCY, 0, 0, 5.0, 0, price, 0]
+    pos_id = client.trade_transaction(*args)['returnData']['order']
+    response = client.trade_transaction_status(pos_id)
+    LOGGER.debug("passed")
     assert response['status'] is True
 
 
@@ -70,5 +189,5 @@ def test_get_user_data(_get_client):
 def test_logout(_get_client):
     client = _get_client
     response = client.logout()
-    print(response)
+    LOGGER.debug("passed")
     assert response['status'] is True
